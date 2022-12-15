f1be4a429aaf5a0d0b3071ec7724470c8e0fb50d
Mário Santos <mariodev7@gmail.com>
2022-12-15 00:56:43 -0300

    create form search

---
+++ src/components/FormSearch/index.tsx
@@ -0,0 +1,81 @@
+import { Button, Flex, Link, Text } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import web3 from "web3";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import { useNfts } from "~/context/NftsContext";
+import { AddressBar } from "./AddressBar";
+import { useEffect } from "react";
+import { toast } from "react-hot-toast";
+
+type FormData = {
+  address: string;
+};
+
+const schema = yup.object().shape({
+  address: yup
+    .string()
+    .required("Wallet address is required")
+    .test(
+      "is-address",
+      "Invalid wallet address",
+      (value) => web3.utils.isAddress(value!) === true
+    ),
+});
+
+export const FormSearch: React.FC = () => {
+  const { getNfts, loading } = useNfts();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    resetField,
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<FormData> = async ({ address }) => {
+    resetField("address");
+    await getNfts(address);
+  };
+
+  useEffect(() => {
+    if (errors.address?.message) toast.error(errors.address.message);
+  }, [errors]);
+
+  return (
+    <Flex
+      as="form"
+      flexDir="column"
+      onSubmit={handleSubmit(onSubmit)}
+      p={6}
+      bg="gray.800"
+      w="full"
+      mb={8}
+      rounded="md"
+      gap={4}
+      justifyContent="center"
+      textAlign="center"
+    >
+      <Text fontSize="lg" textTransform="uppercase" fontWeight="light">
+        This scanner get all nfts by ownership of this{" "}
+        <Link
+          color="pink.500"
+          target="_blank"
+          href="https://etherscan.io/address/0x18df6c571f6fe9283b87f910e41dc5c8b77b7da5"
+        >
+          contract
+        </Link>
+      </Text>
+
+      <Flex gap={2} flexWrap="wrap" justifyContent="center">
+        <AddressBar {...register("address")} />
+        <Button type="submit" colorScheme="pink" isLoading={loading}>
+          Search
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 import { theme } from "../theme";
 import { NftsProvider } from "~/context/NftsContext";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <NftsProvider>
+        <Toaster />
         <Component {...pageProps} />
       </NftsProvider>
     </ChakraProvider>
