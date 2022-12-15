f4d1cf0bc822fe5d0332c8f43f795cc8120ac20f
Mário Santos <mariodev7@gmail.com>
2022-12-15 01:20:42 -0300

    display message not found metamask

--- src/hooks/useWeb3.ts
+++ src/hooks/useWeb3.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Web3 from "web3";
 import { AbiItem } from "web3-utils";
 import { Contract } from "web3-eth-contract";
+import { toast } from "react-hot-toast";
 
 interface useWeb3Props {
   contractABI: AbiItem | AbiItem[];
@@ -14,7 +15,7 @@ export const useWeb3 = ({ contractABI, contractAddress }: useWeb3Props) => {
   useEffect(() => {
     const main = () => {
       if (!contractABI && !contractAddress) return;
-      if (!window.ethereum) return;
+      if (!window.ethereum) return toast.error("We cannot found MetaMask");
 
       const web3 = new Web3(window.ethereum!);
       const newContract = new web3.eth.Contract(contractABI, contractAddress);
