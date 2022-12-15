6d81cd5176dab4302a98a0630ab18e5e69d64a39
Mário Santos <mariodev7@gmail.com>
2022-12-15 01:08:31 -0300

    add fallback to image nft card

--- src/components/NFTCard.tsx
+++ src/components/NFTCard.tsx
@@ -1,4 +1,12 @@
-import { Card, CardBody, Flex, Heading, Image, Stack } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Flex,
+  Heading,
+  Image,
+  Skeleton,
+  Stack,
+} from "@chakra-ui/react";
 
 interface NFTCardProps {
   image: string;
@@ -9,7 +17,13 @@ export const NFTCard: React.FC<NFTCardProps> = ({ image, name }) => {
   return (
     <Card maxW="xs" bg="gray.800" color="white">
       <CardBody as={Flex} flexDir="column" alignItems="center">
-        <Image src={image} alt={name} borderRadius="lg" objectFit="cover" />
+        <Image
+          src={image}
+          alt={name}
+          borderRadius="lg"
+          objectFit="cover"
+          fallback={<Skeleton />}
+        />
         <Stack mt="6" spacing="3">
           <Heading size="md">{name}</Heading>
         </Stack>
